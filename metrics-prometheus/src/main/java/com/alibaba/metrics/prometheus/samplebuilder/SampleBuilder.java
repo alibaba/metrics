685e206d8685578c8e3c42adf8c1f607498d7406
nzomkxia <z82507200@gmail.com>
2019-06-06 17:33:11 +0800

    prometheus data format

---
+++ metrics-prometheus/src/main/java/com/alibaba/metrics/prometheus/samplebuilder/SampleBuilder.java
@@ -0,0 +1,10 @@
+package com.alibaba.metrics.prometheus.samplebuilder;
+
+import com.alibaba.metrics.MetricName;
+import io.prometheus.client.Collector;
+
+import java.util.List;
+
+public interface SampleBuilder {
+    Collector.MetricFamilySamples.Sample createSample(MetricName metricName, String suffix, List<String> labelNames, List<String> labelValues, double value);
+}
