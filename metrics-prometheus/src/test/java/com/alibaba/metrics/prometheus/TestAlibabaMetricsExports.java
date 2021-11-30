cef761e652a1ffa7927015ae2c27809dba18deae
FanYuliang <yf3@illinois.edu>
2021-11-29 22:13:19 -0600

    fix flaky test in TestAlibabaMetricsExports.java

--- metrics-prometheus/src/test/java/com/alibaba/metrics/prometheus/TestAlibabaMetricsExports.java
+++ metrics-prometheus/src/test/java/com/alibaba/metrics/prometheus/TestAlibabaMetricsExports.java
@@ -268,8 +268,8 @@ public class TestAlibabaMetricsExports {
         Assert.assertEquals("prom_test_fastcompass_bucket_count", samples.get(0).samples.get(0).name);
         Assert.assertEquals("success count", 5, samples.get(0).samples.get(0).value, 0.0001d);
         Assert.assertEquals("failure count", 5, samples.get(0).samples.get(1).value, 0.0001d);
-        Assert.assertEquals("success sum", successSum, samples.get(0).samples.get(2).value, 0.0001d);
-        Assert.assertEquals("failure sum", failureSum, samples.get(0).samples.get(3).value, 0.0001d);
+        Assert.assertTrue("success sum", successSum==(int)samples.get(0).samples.get(2).value || successSum==(int)samples.get(0).samples.get(3).value);
+        Assert.assertTrue("failure sum", failureSum==(int)samples.get(0).samples.get(2).value || failureSum==(int)samples.get(0).samples.get(3).value);
     }
 
     @After
